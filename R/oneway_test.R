1ecc5e22f43179bffd0a08e32869c9e95fe83d8d
jklai <jklai@gate.sinica.edu.tw>
2024-04-22 13:14:31 +0800

    update oneway_test

--- R/oneway_test.R
+++ R/oneway_test.R
@@ -25,26 +25,44 @@ oneway_test <- function(data, formula){
     ) %>% 
         tidyr::drop_na()
     
-    stopifnot(sd(df0$y) != 0)
+    is_tied_data <- sd(df0$y) == 0
     
     descriptive_stats <- df0 %>% 
         dplyr::summarise(
             N = length(y),
-            MIN = min(y),
             AVG = mean(y),
+            SD = sd(y),
+            MIN = min(y),
+            P25 = quantile(y, 0.25),
             MED = median(y),
+            P75 = quantile(y, 0.75),
             MAX = max(y),
             .by = x
         ) %>% 
         dplyr::arrange(dplyr::desc(AVG))
+    
+    if (is_tied_data){
+        descriptive_stats$letter <- "a"
+        descriptive_stats <- descriptive_stats %>% 
+            dplyr::rename(indep_var = x)
+        res <- list(
+            normality = NULL, 
+            homoscedasticity = TRUE, 
+            perform_test = NULL,
+            pre_hoc = NULL, 
+            post_hoc = NULL,
+            results = descriptive_stats
+        )
+        return(res)
+    }
     
     # Factor reorder with descending average value
     df0$x <- factor(df0$x, levels = descriptive_stats$x)
     
     # Check data normality and homoscedasticity
     normality <- df0 %>% 
-        group_by(x) %>% 
-        summarise(p = check_normality(y))
+        dplyr::group_by(x) %>% 
+        dplyr::summarise(p = check_normality(y))
     homoscedasticity <- rstatix::levene_test(formula = y ~ x, data = df0)
     is_normal <- all(normality$p > 0.05)
     var_equal <- homoscedasticity$p > 0.05
@@ -54,10 +72,9 @@ oneway_test <- function(data, formula){
         perform_test <- ifelse(var_equal, "Fisher's ANOVA", "Welch's ANOVA")
         pre_hoc <- oneway.test(y ~ x, df0, var.equal = var_equal)
         pre_hoc_pass <- pre_hoc$p.value < 0.05
-        post_hoc <- NULL
         
         # Tukey-HSD
-        if (var_equal & pre_hoc_pass){
+        if (var_equal){
             perform_test <- paste0(perform_test, " + Tukey-HSD")
             post_hoc <- agricolae::HSD.test(
                 y = aov(y ~ x, df0),
@@ -72,7 +89,7 @@ oneway_test <- function(data, formula){
         }
         
         # Games-Howell
-        if(!var_equal & pre_hoc_pass){
+        if(!var_equal){
             perform_test <- paste0(perform_test, " + Games-Howell")
             post_hoc <- rstatix::games_howell_test(
                 data = df0, 
@@ -90,36 +107,39 @@ oneway_test <- function(data, formula){
     
     # Non-parametric test
     if (!is_normal){
-        perform_test <- "Kruskal-Wallis"
+        perform_test <- "Kruskal-Wallis + Dunn's test"
+        
         pre_hoc <- with(
             data = df0,
             expr = kruskal.test(x = y, g = x)
         )
         pre_hoc_pass <- pre_hoc$p.value < 0.05
-        post_hoc <- NULL
         
-        if (pre_hoc_pass){
-            perform_test <- paste0(perform_test, " + Dunn's test")
-            post_hoc <- rstatix::dunn_test(
-                data = df0, 
-                formula = y ~ x
-            ) %>% 
-                dplyr::mutate(comparisons = paste(group1, group2, sep = "-"))
-            
-            cld <- rcompanion::cldList(
-                formula = p.adj ~ comparisons, 
-                data = post_hoc, 
-                remove.zero = FALSE
-            )
-            
-            cld <- data.frame(
-                x = cld[, "Group"],
-                letter = cld[, "Letter", drop = TRUE]
-            )
-        }
+        post_hoc <- rstatix::dunn_test(
+            data = df0, 
+            formula = y ~ x
+        ) %>% 
+            dplyr::mutate(comparisons = paste(group1, group2, sep = "-"))
+        
+        cld <- rcompanion::cldList(
+            formula = p.adj ~ comparisons, 
+            data = post_hoc, 
+            remove.zero = FALSE
+        )
+        
+        cld <- data.frame(
+            x = cld[, "Group"],
+            letter = cld[, "Letter", drop = TRUE]
+        )
     }  # <<<<< Non-parametric test
     
-    if (exists("cld")) descriptive_stats <- descriptive_stats %>% dplyr::left_join(cld, by = "x")
+    if (!pre_hoc_pass) cld$letter <- "a"
+    
+    descriptive_stats <- descriptive_stats %>% 
+        dplyr::left_join(cld, by = "x") %>% 
+        dplyr::rename(indep_var = x)
+    
+    # if (exists("cld")) descriptive_stats <- descriptive_stats %>% dplyr::left_join(cld, by = "x")
     
     res <- list(
         normality = normality, 
