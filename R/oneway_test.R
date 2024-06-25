0d7bf4b218b625fb12ff546dd57b225baa23652c
jklai <jklai@gate.sinica.edu.tw>
2024-06-25 16:54:28 +0800

    add-func: is_normal()

--- R/oneway_test.R
+++ R/oneway_test.R
@@ -5,13 +5,25 @@ suppressMessages({
     if (!require(rcompanion)) install.packages("rcompanion")
     if (!require(agricolae)) install.packages("agricolae")
     if (!require(rstatix)) install.packages("rstatix")
+    
+    options(contrasts = c("contr.helmert", "contr.poly"))
 })
 
-check_normality <- function(x){
-    v <- na.omit(x)
-    if (sd(v) == 0) normality <- TRUE
-    if (sd(v) != 0) normality <- shapiro.test(v)$p.value
-    return(normality)
+is_normal <- function(data, formula, alpha = 0.05){
+    # if the input data is a vector
+    if (length(dim(data)) == 0){
+        v <- na.omit(data)
+        if (sd(v) == 0) res <- FALSE  # Tied data
+        if (sd(v) != 0) res <- shapiro.test(v)$p.value > alpha
+    }
+    
+    # if the input data is a dataframe
+    if (is.data.frame(data)){
+        aov_mod <- aov(formula = formula, data = data)
+        res <- shapiro.test(aov_mod$residuals)$p.value > alpha
+    }
+    
+    return(res)
 }
 
 oneway_test <- function(data, formula, generate_boxplot = TRUE){
